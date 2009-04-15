0370139fcd0121068f350f815a2d0c3c46de1c93
Tekkub <tekkub@gmail.com>
2009-04-14 20:53:48 -0600

    Add sold total

--- ForSaleByOwner.lua
+++ ForSaleByOwner.lua
@@ -7,8 +7,8 @@ local function GS(cash)
 	cash = cash/100
 	local s = floor(cash%100)
 	local g = floor(cash/100)
-	if g > 0 then return string.format("Total buyout: |cffffd700%d.|cffc7c7cf%02d", g, s)
-	else return string.format("Total buyout: |cffc7c7cf%d", s) end
+	if g > 0 then return string.format("|cffffd700%d.|cffc7c7cf%02d", g, s)
+	else return string.format("|cffc7c7cf%d", s) end
 end
 
 
@@ -31,16 +31,33 @@ frame:SetScript("OnEvent", function(self, event, addon)
 		textframe:SetPoint("RIGHT", AuctionsCancelAuctionButton, "LEFT", -5, 0)
 		textframe:SetFontObject(GameFontNormal)
 
+		local label = AuctionsCancelAuctionButton:CreateFontString(nil, "OVERLAY")
+		label:SetPoint("RIGHT", textframe, "LEFT", -5, 0)
+		label:SetFontObject(GameFontHighlight)
+		label:SetText("Total buyout:")
+
+		local textframe2 = AuctionsCancelAuctionButton:CreateFontString(nil, "OVERLAY")
+		textframe2:SetPoint("RIGHT", label, "LEFT", -20, 0)
+		textframe2:SetFontObject(GameFontNormal)
+
+		local label2 = AuctionsCancelAuctionButton:CreateFontString(nil, "OVERLAY")
+		label2:SetPoint("RIGHT", textframe2, "LEFT", -5, 0)
+		label2:SetFontObject(GameFontHighlight)
+		label2:SetText("Total sold:")
+
 		local function OnEvent()
-			local sum = 0
+			local sum, soldsum = 0, 0
 			for i in pairs(chardb) do chardb[i] = nil end
 			for i = 1,GetNumAuctionItems("owner") do
-				local name, _, _, _, _, _, _, _, buyout, _, _, _, sold = GetAuctionItemInfo("owner", i)
+				local name, _, _, _, _, _, _, _, buyout, bidAmount, _, _, sold = GetAuctionItemInfo("owner", i)
 				if sold == 0 then
 					chardb[name] = (chardb[name] or 0) + 1
 					sum = sum + buyout
+				else
+					soldsum = soldsum + bidAmount
 				end
 			end
+			textframe2:SetText(GS(soldsum) or "ERROR")
 			textframe:SetText(GS(sum) or "ERROR")
 		end
 
